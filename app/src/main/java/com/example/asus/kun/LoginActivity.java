164590b1719fb12e8fe4976f1fc107fe85e912e4
asus <995717128@qq.com>
2017-10-19 22:26:59 +0800

    主页及跳转

---
+++ app/src/main/java/com/example/asus/kun/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.asus.kun;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by asus on 2017/10/19.
+ */
+
+public class LoginActivity extends AppCompatActivity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/asus/kun/MainActivity.java
+++ app/src/main/java/com/example/asus/kun/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.asus.kun;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -10,6 +15,23 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+        Button user = (Button) findViewById(R.id.user);
+        user.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ActionBar actionbar = getSupportActionBar();
+
+        if(actionbar != null)
+        {
+            actionbar.hide();
+        }
     }
 }
